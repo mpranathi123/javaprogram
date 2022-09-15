71e092721773375629bac6a2ec985f80a47ed14e
mpranathi123 <113494081+mpranathi123@users.noreply.github.com>
2022-09-15 11:02:46 +0530

    Add files via upload

---
+++ Emplo.java
@@ -0,0 +1,116 @@
+package oop.fr;
+import java.util.Scanner;
+class Emp
+{
+	int age;
+	private String name;
+	float salary;
+	void inp()
+	{
+		Scanner sc=new Scanner(System.in);
+		name=sc.next();
+		age=sc.nextInt();
+		salary=sc.nextFloat();
+	}
+	/**
+	 * 
+	 */
+	/**
+	 * 
+	 */
+	void display()
+	{
+		int total;
+		System.out.println("name:"+name);
+		System.out.println("age:"+age);
+		System.out.println("salary:"+salary);
+		//System.out.println(total);
+	}
+}
+public class Emplo {
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main Method");
+		Emp e1=new Emp();
+		e1.inp();
+		e1.display();
+		
+	}
+
+}
